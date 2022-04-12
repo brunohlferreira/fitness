6ffac81d574ce81ef8a4efb456b9fa350181cb06
Bruno Ferreira <brunohlferreira@gmail.com>
2022-04-12 18:49:32 +0100

    Fix routes

--- routes/web.php
+++ routes/web.php
@@ -21,23 +21,6 @@ Route::get('/', function () {
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('/dashboard', function () {return Inertia::render('Dashboard');})->name('dashboard');
 
-    Route::get('/workouts', [WorkoutController::class, 'index'])->name('workouts.index');
-    Route::get('/workouts/create', [WorkoutController::class, 'create'])->name('workouts.create');
-    Route::get('/workouts/{workout}', [WorkoutController::class, 'show']);
-    Route::get('/workouts/{workout}/edit', [WorkoutController::class, 'edit']);
-    Route::post('/workouts', [WorkoutController::class, 'store']);
-    Route::put('/workouts/{workout}', [WorkoutController::class, 'update']);
-    Route::delete('/workouts/{workout}', [WorkoutController::class, 'destroy']);
-
-    Route::get('/workout-presets', [WorkoutPresetController::class, 'index'])->name('workouts.presets');
-    Route::get('/workout-presets/{workoutPreset}', [WorkoutPresetController::class, 'show'])->name('workouts.presets.show');
-
-    Route::get('/exercises', [ExerciseController::class, 'index'])->name('exercises');
-
-    Route::get('/users', [UserController::class, 'index'])->name('users.index');
-    Route::get('/roles', [RoleController::class, 'index'])->name('roles.index');
-    Route::get('/permissions', [PermissionController::class, 'index'])->name('permissions.index');
-
     Route::get('/body-parts', [BodyPartController::class, 'index'])->name('bodyParts.index');
     Route::get('/body-parts/create', [BodyPartController::class, 'create'])->name('bodyParts.create');
     Route::get('/body-parts/{bodyPart}/edit', [BodyPartController::class, 'edit']);
@@ -52,12 +35,7 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::put('/equipments/{equipment}', [EquipmentController::class, 'update']);
     Route::delete('/equipments/{equipment}', [EquipmentController::class, 'destroy']);
 
-    Route::get('/workout-presets', [WorkoutPresetController::class, 'index'])->name('workoutPresets.index');
-    Route::get('/workout-presets/create', [WorkoutPresetController::class, 'create'])->name('workoutPresets.create');
-    Route::get('/workout-presets/{workoutPreset}/edit', [WorkoutPresetController::class, 'edit']);
-    Route::post('/workout-presets', [WorkoutPresetController::class, 'store']);
-    Route::put('/workout-presets/{workoutPreset}', [WorkoutPresetController::class, 'update']);
-    Route::delete('/workout-presets/{workoutPreset}', [WorkoutPresetController::class, 'destroy']);
+    Route::get('/exercises', [ExerciseController::class, 'index'])->name('exercises');
 
     Route::get('/workout-types', [WorkoutTypeController::class, 'index'])->name('workoutTypes.index');
     Route::get('/workout-types/create', [WorkoutTypeController::class, 'create'])->name('workoutTypes.create');
@@ -65,4 +43,23 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::post('/workout-types', [WorkoutTypeController::class, 'store']);
     Route::put('/workout-types/{workoutType}', [WorkoutTypeController::class, 'update']);
     Route::delete('/workout-types/{workoutType}', [WorkoutTypeController::class, 'destroy']);
+
+    Route::get('/workouts', [WorkoutController::class, 'index'])->name('workouts.index');
+    Route::get('/workouts/create', [WorkoutController::class, 'create'])->name('workouts.create');
+    Route::get('/workouts/{workout}/edit', [WorkoutController::class, 'edit']);
+    Route::get('/workouts/{workout}', [WorkoutController::class, 'show']);
+    Route::post('/workouts', [WorkoutController::class, 'store']);
+    Route::put('/workouts/{workout}', [WorkoutController::class, 'update']);
+    Route::delete('/workouts/{workout}', [WorkoutController::class, 'destroy']);
+
+    Route::get('/workout-presets', [WorkoutPresetController::class, 'index'])->name('workoutPresets.index');
+    Route::get('/workout-presets/create', [WorkoutPresetController::class, 'create'])->name('workoutPresets.create');
+    Route::get('/workout-presets/{workoutPreset}/edit', [WorkoutPresetController::class, 'edit']);
+    Route::post('/workout-presets', [WorkoutPresetController::class, 'store']);
+    Route::put('/workout-presets/{workoutPreset}', [WorkoutPresetController::class, 'update']);
+    Route::delete('/workout-presets/{workoutPreset}', [WorkoutPresetController::class, 'destroy']);
+
+    Route::get('/users', [UserController::class, 'index'])->name('users.index');
+    Route::get('/roles', [RoleController::class, 'index'])->name('roles.index');
+    Route::get('/permissions', [PermissionController::class, 'index'])->name('permissions.index');
 });
